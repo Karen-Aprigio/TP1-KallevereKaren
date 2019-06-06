08f9257530796ff193f0a0809d651932b843a766
kallever10 <kallever10@gmail.com>
2019-06-06 09:14:49 -0300

    Gato.java

---
+++ Gato/src/gato/Gato.java
@@ -0,0 +1,22 @@
+
+package gato;
+
+public class Gato {
+siames femea;
+String nome;
+int idade;
+Gato mae;
+public Gato (String n){
+    this.nome= n;
+}
+public mia () {
+    if (idade>=2) {
+        System.out.print ("meow meow meow!");
+    }else{
+        System.out.print ("miau miau miau");
+    }
+    
+}
+
+    
+}
