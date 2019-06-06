bac62d2f39048630cf97d8446b638b864716ecbe
KKaren <karencoutinhoaprigio08@gmail.com>
2019-06-06 10:36:51 -0300

    atividades

---
+++ Gato-KARENAQ/src/gato/Gato.java
@@ -0,0 +1,22 @@
+
+package gato;
+
+public class Gato {
+boolean sexo;
+String nome;
+int idade;
+int codigoderegistro;
+Gato mae;
+public Gato (String n){
+    this.nome= n;
+}
+public void miar () {
+    if (idade>=2) {
+        System.out.print ("meow meow meow!");
+    }else{
+        System.out.print ("miau miau miau");
+    }
+}
+
+    
+}
---
+++ Gato-KARENAQ/src/gato/Parque.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gato;
+
+/**
+ *
+ * @author aluno
+ */
+public class Parque {
+
+   public static int main (String[]args){
+       Gato velho = new Gato("Lucas");
+       velho.miar();
+       Gato novo = new Gato("Maria");
+       novo.miar();
+       return 0;
+      
+   }
+    
+}
